51c5e7f5903c3af4c4ced3515d718dff4e923b4d
Elan Ruusamäe <glen@delfi.ee>
2014-01-31 22:42:50 +0200

    codestyle
    - use strings instead of symbols
    - use single quotes where possible

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,21 +1,21 @@
 # Base settings
-default[:gitweb][:group] = "www-data"
-default[:gitweb][:config] = "/etc/gitweb.conf"
-default[:gitweb][:config_directory] = "/etc/gitweb"
-default[:gitweb][:project_root] = "/home/git/repositories"
-default[:gitweb][:base_url_list] = []
-default[:gitweb][:server_name] = "git.#{node[:domain]}"
-default[:gitweb][:server_root] = "/usr/share/gitweb"
+default['gitweb']['group'] = 'www-data'
+default['gitweb']['config'] = '/etc/gitweb.conf'
+default['gitweb']['config_directory'] = '/etc/gitweb'
+default['gitweb']['project_root'] = '/home/git/repositories'
+default['gitweb']['base_url_list'] = []
+default['gitweb']['server_name'] = "git.#{node[:domain]}"
+default['gitweb']['server_root'] = '/usr/share/gitweb'
 
 # Nginx settings
-default[:gitweb][:nginx_host] = nil
-default[:gitweb][:nginx_port] = 80
-default[:gitweb][:nginx_http_to_https] = true
-default[:gitweb][:nginx_default_server] = false
-default[:gitweb][:nginx_ssl] = false
-default[:gitweb][:nginx_ssl_days] = 365 * 5
-default[:gitweb][:nginx_ssl_key_type] = "rsa"
-default[:gitweb][:nginx_ssl_key_size] = 2048
+default['gitweb']['nginx_host'] = nil
+default['gitweb']['nginx_port'] = 80
+default['gitweb']['nginx_http_to_https'] = true
+default['gitweb']['nginx_default_server'] = false
+default['gitweb']['nginx_ssl'] = false
+default['gitweb']['nginx_ssl_days'] = 365 * 5
+default['gitweb']['nginx_ssl_key_type'] = 'rsa'
+default['gitweb']['nginx_ssl_key_size'] = 2048
 
 # Theme settings
-default[:gitweb][:theme] = "https://github.com/kogakure/gitweb-theme.git"
+default['gitweb']['theme'] = 'https://github.com/kogakure/gitweb-theme.git'
--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer        "Nickolay Kovalev"
-maintainer_email  "nickola@nickola.ru"
-license           "Apache 2.0"
-description       "Gitweb installation and configuration"
+maintainer        'Nickolay Kovalev'
+maintainer_email  'nickola@nickola.ru'
+license           'Apache 2.0'
+description       'Gitweb installation and configuration'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0"
+version           '1.0'
 
 %w{git nginx}.each do |cb|
   depends cb
